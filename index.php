88ea9164cccd42c8bd6165a5c9282c3cdead6d29
Andsirpa <andsirpa94@gmail.com>
2024-03-05 18:57:44 +0100

    add: created function generate_password

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+// creo la funzione per generare una password
+function generate_password($length)
+{
+    // con 'range' prendo i caratteri compresi
+    $characters = range('a', 'z') . range('A', 'Z') . range('0', '9');
+    $password = "";
+
+    // genero una password casuale
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $password;
+}
+
